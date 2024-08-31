dcd8c82d8cf477fd1a5fca6132b28474291c10c5
David Barnett <david@mumind.me>
2024-08-31 13:45:40 -0600

    Implement parse_plugin_dir

--- lib/src/data.rs
+++ lib/src/data.rs
@@ -3,3 +3,14 @@ pub enum VimNode {
     StandaloneDocComment(String),
     Function { name: String, doc: Option<String> },
 }
+
+#[derive(Debug, PartialEq)]
+pub struct VimPluginSection {
+    pub name: String,
+    pub nodes: Vec<VimNode>,
+}
+
+#[derive(Debug, PartialEq)]
+pub struct VimPlugin {
+    pub content: Vec<VimPluginSection>,
+}
